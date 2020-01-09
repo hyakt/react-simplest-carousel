ebe362d8d66adef51ae7b166fcad4b0e68ef0336
hyakt <hyakt0@gmail.com>
2020-01-09 15:11:29 +0900

    update: image to react element

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,20 +1,13 @@
-import * as React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import styles from './Carousel.module.scss'
 
-type Props = {imgUrls: Array<string>}
+type Props = {elements: Array<{}>}
 
-const ImageSlide = ({url}) => {
-  console.log(url)
-
-  const styles = {
-    backgroundImage: `url(${url})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '400px',
-    transition: 'background-image .3s ease-in-out',
-  }
-
+const Gallery = ({element}) => {
   return (
-    <div className="image-slide" style={styles}></div>
+    <div className={styles.gallery}>
+      {element}
+    </div>
   )
 }
 
@@ -26,63 +19,82 @@ const Arrow = ({ direction, clickFunction, glyph }) => (
   </div>
 )
 
-const ThumbnailList = ({imgUrls, callback}) => {
+const calculateOffsetX = (containerWidth, elementsLength) => {
+  return containerWidth / elementsLength
+}
 
-  const styles = {
-    flex: '0 0 auto',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '200px',
-    width: '200px',
-    transition: 'transform 100ms ease-out',
-    minWidth: '0',
-  }
+const ThumbnailList = ({elements, callback}) => {
+  const [style, setStyle] = useState({});
+  const [containerWidth, setContainerWidth] = useState(0);
 
-  const [style, setStyle] = React.useState(styles)
+  const ulList = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    setContainerWidth(ulList?.current?.offsetWidth || 0);
+  }, [ulList.current]);
+
+  const thumbnailWidth = 100;
 
   const onClick = (index) => {
     callback(index)
-    setStyle({...styles, ...{transform: `translate3d(${index * -180}px, 0, 0)`}})
+    const offsetX = calculateOffsetX(containerWidth, elements.length) * -1 * index;
+    setStyle({transform: `translate3d(${offsetX}px, 0, 0`})
   }
 
   return (
-    <ul className="image-list">
-      {imgUrls.map((url, index) => {
-        return <li style={{backgroundImage: `url(${url})`, ...style}} onClick={() => onClick(index)} />
-      })}
-    </ul>
+    <div className={styles.thumbnailContainer}>
+      <ul ref={ulList} className={styles.thumbnailList}>
+        {elements.map((element, index) => {
+          console.log(element)
+          return (
+            <li className={styles.thumbnailContent}
+                style={{...style}}
+                onClick={() => onClick(index)}>
+              {element}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
 const Carousel = (props: Props) => {
-  const [currentImageIndex, setCurrentImageIndex] = React.useState<number>(0)
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0)
 
   const previousSlide = () => {
-    const lastIndex = props.imgUrls.length - 1
+    const lastIndex = props.elements.length - 1
     const shouldResetIndex = currentImageIndex === 0
     const index = shouldResetIndex ? lastIndex: currentImageIndex -1
     setCurrentImageIndex(index)
   }
 
   const nextSlide = () => {
-    const lastIndex = props.imgUrls.length - 1
+    const lastIndex = props.elements.length - 1
     const shouldResetIndex = currentImageIndex === lastIndex
     const index = shouldResetIndex ? 0 : currentImageIndex + 1
     setCurrentImageIndex(index)
   }
 
   return (
-    <div className="carousel">
-      <Arrow
-        direction="left"
-        clickFunction={ previousSlide }
-        glyph="&#9664;" />
-      <ImageSlide url={ props.imgUrls[currentImageIndex] } />
-      <ThumbnailList imgUrls={props.imgUrls} callback={setCurrentImageIndex} />
-      <Arrow
-        direction="right"
-        clickFunction={ nextSlide }
-        glyph="&#9654;" />
+    <div className={styles.carousel}>
+      {/*
+          <Arrow
+          direction="left"
+          clickFunction={ previousSlide }
+          glyph="&#9664;" />
+        */}
+      <Gallery
+        element={props.elements[currentImageIndex]} />
+      <ThumbnailList
+        elements={props.elements}
+        callback={setCurrentImageIndex} />
+      {/*
+          <Arrow
+          direction="right"
+          clickFunction={ nextSlide }
+          glyph="&#9654;" />
+        */}
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Carousel from './Carousel';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import Carousel from './Carousel'
 
-const imgUrls = [
-  'https://via.placeholder.com/200/00FFFF/000000/',
-  'https://via.placeholder.com/200/00FF00/000000/',
-  'https://via.placeholder.com/200/ff0000/000000/',
-  'https://via.placeholder.com/200/0000ff/000000/',
-  'https://via.placeholder.com/200/F0F0FF/000000/',
-  'https://via.placeholder.com/200/F92672/000000/',
-  'https://via.placeholder.com/200/A6E22E/000000/',
-  'https://via.placeholder.com/200/FD971F/000000/',
-  'https://via.placeholder.com/200/66D9EF/000000/',
-  'https://via.placeholder.com/200/AE81FF/000000/',
-  'https://via.placeholder.com/200/E6DB74/000000/',
-  'https://via.placeholder.com/200/75715E/000000/',
-  'https://via.placeholder.com/200/272822/000000/',
+const getRandomColorCode = () => `#${'0123456789ABCDEF'.split('').sort(() => 0.5 - Math.random()).slice(0,6).join('')}`
+
+const elements = () => [
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
+  <div className='thumbnail-image' style={{background: `${getRandomColorCode()}`}} />,
 ]
 
-ReactDOM.render(<Carousel imgUrls={imgUrls}/>, document.getElementById('root'));
+ReactDOM.render(<Carousel elements={elements()}/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// serviceWorker.unregister()
